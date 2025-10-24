276e18f854fb1870284b2bf8ddbdc9a117fe6ef0
Sebastian CARON <sebastiancaron62@gmail.com>
2025-10-24 21:46:26 +0200

    :pencil2: + post_player_register code

--- server/src/endpoints/player/register/register.c
+++ server/src/endpoints/player/register/register.c
@@ -1,22 +1,53 @@
+#include <stdio.h>
+
 #include "../../../network/client.h"
 #include "../../../network/network.h"
 #include "../../endpoints.h"
 #include "../../../db/db.h"
-#include <stdio.h>
+#include "../../../json/cJSON.h"
 
-int post_player_register(server *s, char *requets, client *cl){
-    char *request[1024] = {'\0'};
+char *get_from_json(cJSON *json, char *key){
+    cJSON *value = cJSON_GetObjectItemCaseSensitive(json, key);
+    if (cJSON_IsString(value) && (value->valuestring != NULL)) {
+        return value->valuestring;
+    }
+    return NULL;
+}
+
+int post_player_register(server *s, char *request, client *cl){
+    char query[1024] = {'\0'};
     char *base = "";
-    snprintf(request, 1023, "INSERT INTO clients(pseudo, password) VALUES (%s, %s);", 
-        get_from_json(request, "pseudo"), get_from_json(request, "password"));
-    
-    SqliteResult *res = exec_query(s, request);
-    
-    if(!res){
-        // RETOURNER ERREUR
+
+    cJSON *json = cJSON_Parse(request);
+    if (json == NULL) {
+        const char *error_ptr = cJSON_GetErrorPtr();
+        if (error_ptr != NULL) {
+            printf("Error: %s\n", error_ptr);
+        }
+        cJSON_Delete(json);
         return 1;
     }
-
+    
+    // TODO: Hash password
+    snprintf(query, 1023, "INSERT INTO clients(pseudo, password) VALUES (%s, %s);", 
+        get_from_json(json, "pseudo"), get_from_json(json, "password"));
+    cJSON_Delete(json);
     
 
+    SqliteResult *res = exec_query(s, query);
+    if(!res){
+        throw_error(DB_QUERY, "Erreur post_player_register");
+        send_error_response(cl);
+        return 1;
+    }
+    
+    const char *response =
+    "{"
+    "   \"action\":\"player/register\",\n"
+    "   \"statut\":\"201\",\n"
+    "   \"message\":\"player registered successfully\"\n"
+    "}";
+    send_response(cl, response);
+
+    return 0;
 }
