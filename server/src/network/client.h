c1ff9565d98218069a5f26016980aea4988f6533
Sebastian CARON <sebastiancaron62@gmail.com>
2025-10-25 13:50:31 +0200

    :wrench: fix struct order in client.h

--- server/src/network/client.h
+++ server/src/network/client.h
@@ -1,17 +1,17 @@
 #pragma once
 
 typedef struct {
+    int score;
+    char joker_pass;
+    char joker_5050;
+} infos_session_client;
+
+typedef struct {
     int fd;
     char connected;
     char* pseudo;
     infos_session_client infos_session;
 } client;
-
-typedef struct {
-    int score;
-    char joker_pass;
-    char joker_5050;
-} infos_session_client;
 
 client *client_init(int fd);
 
