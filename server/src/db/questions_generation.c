82d41f914a74a59db31a355fd3d473c2703ec79b
Sebastian CARON <sebastiancaron62@gmail.com>
2025-10-25 14:18:36 +0200

    :wrench: fix questions_generation

--- server/src/db/questions_generation.c
+++ server/src/db/questions_generation.c
@@ -1,19 +1,24 @@
+#include <string.h>
+#include <stdlib.h>
+
 #include "questions_generation.h"
 
-void get_questions(int size, int* tab, int nb_themes, int* ids_themes){
+void get_questions(server *s, int size, int* tab, int nb_themes, int* ids_themes){
     int i;
     SqliteResult* resquery;
-    char query = malloc(512*sizeof(char));
-    char* debut_query = "SELECT DISTINCT q.id FROM questions "
+    char query[512] = {'\0'};
+    char number[64] = {'\0'};
+    char *debut_query = "SELECT DISTINCT q.id FROM questions "
                         "NATURAL JOIN question_in_theme "
                         "NATURAL JOIN themes t "
                         "WHERE t.id IN (";
-
-    strcat(query, itoa(ids_themes[0]));
+    itoa(ids_themes[0], number, 10);
+    strcat(query, number);
 
     for (i =1; i < nb_themes; i++){
-        query = strcat(query, ",")
-        query = strcat(itoa(ids_themes[i]))
+        strcat(query, ",");
+        itoa(ids_themes[i], number, 10);
+        strcat(query, number);
     }
 
     strcat(query, ") ORDER BY RAND() LIMIT ");
@@ -22,6 +27,6 @@ void get_questions(int size, int* tab, int nb_themes, int* ids_themes){
     resquery = exec_query(s, query);
 
     for (i = 0; i < size; i++){
-        tab[i] = atoi(resquery->row[i][0])
+        tab[i] = atoi(resquery->rows[i][0]);
     }
 }
