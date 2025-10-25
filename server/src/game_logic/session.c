957d0c7b20838817eced50c5d16c7ba772ba38eb
Sebastian CARON <sebastiancaron62@gmail.com>
2025-10-25 14:40:04 +0200

    :wrench: fix difficulty

--- server/src/game_logic/session.c
+++ server/src/game_logic/session.c
@@ -2,12 +2,12 @@
 #include "session.h"
 
 session_type get_session_type(char* mode){
-    return strcmp("solo", mode) ? session_type.BATTLE : session_type.CLASSIC;
+    return strcmp("solo", mode) ? BATTLE : CLASSIC;
 }
 
-difficulty get_session_difficulty(char* difficutly){
-    if (!(strcmp("moyen", difficulty))) return difficulty.MEDIUM;
-    if (!(strcmp("difficile", difficulty))) return difficulty.HARD;
-    return difficulty.EASY;
+difficulty get_session_difficulty(char* difficulty_str){
+    if (!(strcmp("moyen", difficulty_str))) return MEDIUM;
+    if (!(strcmp("difficile", difficulty_str))) return HARD;
+    return EASY;
 }
 
