42a35b20e1b69f79a60b56268d2170b7936c6594
Sebastian CARON <sebastiancaron62@gmail.com>
2025-11-01 18:12:30 +0100

    :pencil2: to_string functions for enum from session

--- server/src/game_logic/session.h
+++ server/src/game_logic/session.h
@@ -11,6 +11,10 @@ session_type get_session_type(char* mode);
 
 difficulty get_session_difficulty(char* difficulty);
 
+const char* difficulty_to_string(difficulty d);
+const char* session_type_to_string(session_type t);
+const char* session_status_to_string(session_status s);
+
 void session_destroy(session *);
 
 session* get_session_by_id(chained_list* l_sessions, int id_session);
