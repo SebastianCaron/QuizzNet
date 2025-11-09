2fac89cefb63cff26981ac5841e929a221f29927
Sebastian CARON <sebastiancaron62@gmail.com>
2025-11-09 17:03:13 +0100

    :pencil: send session is over to client

---
+++ server/src/game_logic/session_responses/session_finished.c
@@ -0,0 +1,117 @@
+#include <string.h>
+#include <stdlib.h>
+
+#include "session_finished.h"
+#include "../../endpoints/endpoints.h"
+#include "../../utils/chained_list.h"
+#include "../../json/cJSON.h"
+#include "../session.h"
+#include "../../errors/error.h"
+
+static int compare_solo(const void *a, const void *b) {
+    client *client_a = *(client **)a;
+    client *client_b = *(client **)b;
+    
+    if(client_b->infos_session.score != client_a->infos_session.score) {
+        return client_b->infos_session.score - client_a->infos_session.score;
+    }
+    
+    return client_b->infos_session.correctAnswers - client_a->infos_session.correctAnswers;
+}
+
+static int compare_battle(const void *a, const void *b) {
+    client *client_a = *(client **)a;
+    client *client_b = *(client **)b;
+    
+    if(client_b->infos_session.lives != client_a->infos_session.lives) {
+        return client_b->infos_session.lives - client_a->infos_session.lives;
+    }
+    
+    if(client_a->infos_session.eliminatedAt == 0 && client_b->infos_session.eliminatedAt != 0) {
+        return -1;
+    }
+    if(client_a->infos_session.eliminatedAt != 0 && client_b->infos_session.eliminatedAt == 0) {
+        return 1;
+    }
+    
+    if(client_a->infos_session.eliminatedAt != 0 && client_b->infos_session.eliminatedAt != 0) {
+        return client_b->infos_session.eliminatedAt - client_a->infos_session.eliminatedAt;
+    }
+    
+    return client_b->infos_session.score - client_a->infos_session.score;
+}
+
+void send_session_finished(session *s){
+    if(!s) return;
+    
+    int nb_players = clist_size(s->players);
+    client **players_array = malloc(nb_players * sizeof(client *));
+    if(!players_array) {
+        throw_error(MEMORY_ALLOCATION, "Erreur allocation pour players_array dans send_session_finished");
+        return;
+    }
+    
+    for(int i = 0; i < nb_players; i++) {
+        players_array[i] = (client *)clist_get(s->players, i);
+    }
+    
+    if(s->type == CLASSIC) {
+        qsort(players_array, nb_players, sizeof(client *), compare_solo);
+    } else {
+        qsort(players_array, nb_players, sizeof(client *), compare_battle);
+    }
+    
+    cJSON *response_json = cJSON_CreateObject();
+    cJSON_AddStringToObject(response_json, "mode", session_type_to_string(s->type));
+    
+    if(s->type == BATTLE && nb_players > 0 && players_array[0]) {
+        cJSON_AddStringToObject(response_json, "winner", players_array[0]->pseudo ? players_array[0]->pseudo : "");
+    }
+    
+    cJSON *ranking_array = cJSON_CreateArray();
+    
+    for(int i = 0; i < nb_players; i++) {
+        client *c = players_array[i];
+        if(!c) continue;
+        
+        cJSON *player_rank = cJSON_CreateObject();
+        cJSON_AddNumberToObject(player_rank, "rank", i + 1);
+        cJSON_AddStringToObject(player_rank, "pseudo", c->pseudo ? c->pseudo : "");
+        
+        if(s->type == CLASSIC) {
+            cJSON_AddNumberToObject(player_rank, "score", c->infos_session.score);
+            cJSON_AddNumberToObject(player_rank, "correctAnswers", c->infos_session.correctAnswers);
+        } else {
+            cJSON_AddNumberToObject(player_rank, "lives", c->infos_session.lives);
+            cJSON_AddNumberToObject(player_rank, "score", c->infos_session.score);
+            cJSON_AddNumberToObject(player_rank, "correctAnswers", c->infos_session.correctAnswers);
+            
+            if(c->infos_session.eliminatedAt > 0) {
+                cJSON_AddNumberToObject(player_rank, "eliminatedAt", c->infos_session.eliminatedAt);
+            }
+        }
+        
+        cJSON_AddItemToArray(ranking_array, player_rank);
+    }
+    
+    cJSON_AddItemToObject(response_json, "ranking", ranking_array);
+    
+    char *json_string = cJSON_Print(response_json);
+    if(json_string) {
+        char response[4096] = {'\0'};
+        snprintf(response, sizeof(response), "POST session/finished\n%s", json_string);
+        
+        for(int i = 0; i < nb_players; i++) {
+            client *c = players_array[i];
+            if(c) {
+                send_response(c, response);
+            }
+        }
+        
+        free(json_string);
+    }
+    
+    cJSON_Delete(response_json);
+    free(players_array);
+}
+
