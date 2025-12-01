850fafbebad332b8eb208e177022da2adca5e73b
Sebastian CARON <sebastiancaron62@gmail.com>
2025-12-01 10:16:38 +0100

    :pencil: handle ctrl c

--- server/src/main.c
+++ server/src/main.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <signal.h>
+#include <unistd.h>
+#include <pthread.h>
 
 #include "network/udp.h"
 #include "network/network.h"
@@ -10,6 +13,49 @@
 #include "errors/error.h"
 
 #define UDP_PORT 5555
+
+static server *g_server = NULL;
+
+void cleanup_all_resources(void) {
+    server_state *state = get_server_state();
+    
+    if (!state || !state->srv) {
+        return;
+    }
+    
+    info_log("Arrêt du serveur en cours...");
+    
+    if (state->srv->server_fd_tcp >= 0) {
+        close(state->srv->server_fd_tcp);
+        state->srv->server_fd_tcp = -1;
+    }
+    
+    if (state->srv->clients) {
+        int client_count = clist_size(state->srv->clients);
+        for (int i = client_count - 1; i >= 0; i--) {
+            client *cl = clist_pop(state->srv->clients, i);
+            if (cl) {
+                client_destroy(cl);
+            }
+        }
+    }
+    
+    pthread_join(state->udp_thread, NULL);
+    
+    if (state->srv->sessions) {
+        sleep(1);
+    }
+    close_db(state->srv);
+    destroy_server(state->srv);
+    info_log("Serveur arrêté.");
+}
+
+void signal_handler(int sig) {
+    if (sig == SIGINT) {
+        server_state *state = get_server_state();
+        state->should_stop = 1;
+    }
+}
 
 int main(int argc, char *argv[]) {
     if (argc < 3) {
@@ -25,14 +71,28 @@ int main(int argc, char *argv[]) {
         init_debug_log();
     }
 
+    signal(SIGINT, signal_handler);
+
     start_udp(SERVER_NAME, PORT_TCP);
 
-    server *s = start_server(atoi(argv[2]));
-    add_sqlite_to_server(s);
-    init_db(s);
-    while(1){
-        server_client_procedure(s);
+    g_server = start_server(atoi(argv[2]));
+    if (!g_server) {
+        fprintf(stderr, "Erreur lors du démarrage du serveur\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    init_server_state(g_server);
+    add_sqlite_to_server(g_server);
+    init_db(g_server);
+    
+    server_state *state = get_server_state();
+    while(!state->should_stop){
+        server_client_procedure(g_server);
+        usleep(100000);
     }
 
+    cleanup_all_resources();
     close_debug_log();
+    
+    return EXIT_SUCCESS;
 }
